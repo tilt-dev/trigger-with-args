a2410d98bd47aa9a25d07842c420cf889e392a8c
Maia <maia.mcc@gmail.com>
2021-05-12 11:30:21 -0400

    get cmd for resource if exists

--- cmd/main.go
+++ cmd/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"flag"
 	"fmt"
 	"os"
 	"strings"
@@ -14,28 +13,52 @@ import (
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
+func parseArgs() (resource string, args []string, err error) {
+	if len(os.Args) < 3 {
+		return "", nil,
+			fmt.Errorf("not enough args (need resource name and at minimum one arg to pass to its Cmd). Got args: %v", os.Args[1:])
+	}
+	return os.Args[1], os.Args[2:], nil
+}
 func run() error {
-	flag.Parse()
+	// flag.Parse()
 
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	fmt.Println("hello world")
-	fmt.Println("here are all cmds:")
-	cmds, err := allCmdsByName(ctx)
+	resource, newArgs, err := parseArgs()
 	if err != nil {
 		return err
 	}
 
-	for n, c := range cmds {
-		fmt.Println(n)
-		fmt.Printf("\t%s\n", strings.Join(c.Spec.Args, " "))
-		fmt.Println("---")
+	cmd, err := cmdForResource(ctx, resource)
+	if err != nil {
+		return err
 	}
+
+	fmt.Printf("For resource %s, found command:\n", resource)
+	fmt.Printf("\t%s\n", strings.Join(cmd.Spec.Args, " "))
+	fmt.Printf("will call with args: %s\n", strings.Join(newArgs, " "))
 	return nil
 }
 
-func allCmdsByName(ctx context.Context) (map[string]v1alpha1.Cmd, error) {
+func cmdForResource(ctx context.Context, resource string) (v1alpha1.Cmd, error) {
+	cmds, err := allCmdsByResource(ctx)
+	if err != nil {
+		return v1alpha1.Cmd{}, errors.Wrap(err, "getting all Cmds")
+	}
+	cmd, ok := cmds[resource]
+	if !ok {
+		var allResources []string
+		for resource := range cmds {
+			allResources = append(allResources, resource)
+		}
+		return v1alpha1.Cmd{}, fmt.Errorf("no Cmd found for resource %q (found Cmds for the following resources: %s)",
+			resource, strings.Join(allResources, ", "))
+	}
+	return cmd, nil
+}
+func allCmdsByResource(ctx context.Context) (map[string]v1alpha1.Cmd, error) {
 	// TODO - how do we handle multiple tilt instances?
 	cfg, err := config.NewConfig()
 	if err != nil {
@@ -51,7 +74,7 @@ func allCmdsByName(ctx context.Context) (map[string]v1alpha1.Cmd, error) {
 
 	ret := make(map[string]v1alpha1.Cmd, len(cmds.Items))
 	for _, cmd := range cmds.Items {
-		ret[cmd.Name] = cmd
+		ret[cmd.ObjectMeta.Annotations[v1alpha1.AnnotationManifest]] = cmd
 	}
 	return ret, nil
 }
@@ -59,7 +82,7 @@ func allCmdsByName(ctx context.Context) (map[string]v1alpha1.Cmd, error) {
 func main() {
 	err := run()
 	if err != nil {
-		_, _ = fmt.Fprintf(os.Stderr, "%v", err)
+		_, _ = fmt.Fprintf(os.Stderr, "%v\n", err)
 		os.Exit(1)
 	}
 }
